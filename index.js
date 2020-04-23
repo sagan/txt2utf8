884c81e543f64c7176553d2d5d55976e4f31c18e
Jacques De SAGAN <jacquesde@sagan.me>
2020-04-23 10:31:10 +0800

    allow convert files with type other than txt

--- index.js
+++ index.js
@@ -14,6 +14,17 @@ async function main(argv) {
     .arguments("<rootPath>")
     .option("-d, --debug", "output extra debugging")
     .option(
+      "--min <min_size>",
+      "only convert files of at least this size.",
+      parseFloat,
+      100
+    )
+    .option(
+      "-t, --type <file_type>",
+      "convert files of these comma seperated exension(s).",
+      "txt"
+    )
+    .option(
       "-r, --rename-unrecognized",
       "rename files which encoding is unrecognized with prefix '__unknown_encoding__'"
     )
@@ -24,10 +35,32 @@ async function main(argv) {
   if (program.debug) console.log(program.opts());
 
   let count = 0;
+  let fileFilter = program.type.split(/\s*,\s*/).reduce((fileFilter, type) => {
+    if (type.startsWith(".")) {
+      type = type.slice(1);
+    }
+    if (!type) {
+      return fileFilter;
+    }
+    type = type.toLowerCase();
+    fileFilter.push(`*.${type}`);
+    fileFilter.push(`*.${type.toUpperCase()}`);
+    if (type.length > 1) {
+      fileFilter.push(`*.${type[0].toUpperCase() + type.slice(1)}`);
+    }
+    return fileFilter;
+  }, []);
+  if (fileFilter.length == 0) {
+    fileFilter.push("*.txt", "*.TXT", "*.Txt");
+  }
   const { confirm } = await prompt({
     type: "input",
     name: "confirm",
-    message: `Will convert all *.txt files in ${root} to UTF-8. Are you sure to continue ? (y/n)`
+    message: `Will convert all ${fileFilter.join(
+      ","
+    )} files (of at least size ${
+      program.min
+    }) in ${root} to UTF-8. Are you sure to continue ? (y/n)`
   });
   if (confirm != "y" && confirm != "Y") {
     console.log("Abort");
@@ -35,10 +68,10 @@ async function main(argv) {
   }
   for await (const entry of readdirp(root, {
     alwaysStat: true,
-    fileFilter: ["*.txt", "*.TXT", "*.Txt"]
+    fileFilter
   })) {
     const { path: p, fullPath, basename, stats } = entry;
-    if (stats.size < 100) {
+    if (stats.size < program.min) {
       // console.log(`skip small file ${p}`);
       continue;
     } else if (stats.size >= 100 * 1024 * 1024) {
